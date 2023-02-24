13965922d838e8037c1b2f4a2f46fe1daab13cc4
amchampoux <amchampoux@mirego.com>
2023-02-23 23:42:22 -0500

    complete form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 
 storiesOf("Button", module)
@@ -179,4 +180,18 @@ storiesOf("Button", module)
           <Error 
             message="Could not delete appointment"
             onClose={action("onClose")}
-          />));
+          />))
+        .add("Create", () => (
+          <Form 
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />))
+        .add("Edit", () => (
+          <Form 
+            student="Anne-Marie Champoux"
+            interviewer={3}
+            interviewers={interviewers}
+            onSave={action("onSave")}
+            onCancel={action("onCancel")}
+          />));    
