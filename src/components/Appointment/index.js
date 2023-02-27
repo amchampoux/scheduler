72b1bbc5f841d006c25f84af290c11323e3016d3
amchampoux <amchampoux@mirego.com>
2023-02-26 22:03:05 -0500

    add Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,28 @@
-import React from "react";
-import "components/Appointment/styles.scss";
+import React, { Fragment } from 'react'
 
+import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <Fragment >
+      <Header  time={props.time} />
+        <article className="appointment">
+        {props.interview ? (
+          <Show 
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.name} 
+            /> 
+        ) : (
+          <Empty />
+        )}
+        </article>
+    </Fragment>
+
   );
 }
+
+
