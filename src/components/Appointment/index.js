f4006127e801b4137018ec73c8c69c84feaef5dd
amchampoux <amchampoux@mirego.com>
2023-03-03 17:13:58 -0500

    create useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,21 +3,39 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
+
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <Fragment>
       <Header time={props.time} />
         <article className="appointment"> 
-        {props.interview ? (
+        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        )}
+        {/* {props.interview ? (
+          useVisualMode(SHOW) 
           <Show 
             student={props.interview.student}
             interviewer={props.interview.interviewer.name} 
             /> 
         ) : (
           <Empty />
-        )} 
+        )}  */}
         </article>
     </Fragment>
   );
