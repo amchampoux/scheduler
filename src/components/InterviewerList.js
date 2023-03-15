1340caa839b41dc2b77f0f92055ee2b0752d9c0b
amchampoux <amchampoux@mirego.com>
2023-03-15 11:22:11 -0400

    Add Prop Type library

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import 'components/InterviewerList.scss';
 import InterviewerListItem from "./InterviewerListItem";
 
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
 
   const mappedInterviewers = props.interviewers.map((interviewer) => {
     return <InterviewerListItem 
@@ -24,3 +25,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
